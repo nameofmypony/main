592c7ae56cb4754cb15ed212de60b68f7a666a30
nameofmypony <romanbezverhnij@gmail.com>
2024-09-25 22:31:27 +0600

    Update Лаб 1.cs

--- 1сем/Лаб 1.cs
+++ 1сем/Лаб 1.cs
@@ -1,1 +1,26 @@
-
+using System;
+class Q
+{
+    static void Main()
+    {
+        int a,b;
+        a=Convert.ToInt32(Console.ReadLine());
+        b=Convert.ToInt32(Console.ReadLine());
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        Console.WriteLine("a = "+a);
+        Console.WriteLine("b = "+b);
+        
+        a=(a+b+Math.Abs(a-b))/2;
+        Console.WriteLine("max = "+a);
+        
+        int p,l,m,n,q;
+        p=Convert.ToInt32(Console.ReadLine());
+        l=Convert.ToInt32(Console.ReadLine());
+        m=Convert.ToInt32(Console.ReadLine());
+        n=Convert.ToInt32(Console.ReadLine());
+        q=n*(2*(p+l+m)+l*(n-1));
+        Console.WriteLine("distanse = "+q);
+    }
+}
