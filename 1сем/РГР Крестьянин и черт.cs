3114c1604e0ab37cbb7f90b1e57f234dc7d4ae6e
nameofmypony <romanbezverhnij@gmail.com>
2024-12-08 21:44:35 +0600

    Update РГР Крестьянин и черт.cs

--- 1сем/РГР Крестьянин и черт.cs
+++ 1сем/РГР Крестьянин и черт.cs
@@ -1,13 +1,19 @@
 using System;
-class Q {
-    static void Main() {
+class Q
+{
+    static void Main()
+    {
         int MaxN = int.Parse(Console.ReadLine()), count = 0;
-        for (int N = 1; N <= MaxN; N++) {
-            for (int K = N + 1; K <= MaxN * 2; K++) {
+        for (int N = 1; N <= MaxN; N++)
+        {
+            for (int K = N + 1; K <= MaxN * 2; K++)
+            {
                 int t = N;
-                while (t >= 0) {
+                while (t >= 0)
+                {
                     t = t * 2 - K;
-                    if (t == 0) {
+                    if (t == 0)
+                    {
                         count++;
                         break;
                     }
