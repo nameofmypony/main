dedfa2c629752daa59d8bca0a7759b1c622c8d29
nameofmypony <romanbezverhnij@gmail.com>
2024-12-08 21:44:13 +0600

    Update РГР Отгадай число.cs

--- 1сем/РГР Отгадай число.cs
+++ 1сем/РГР Отгадай число.cs
@@ -1,20 +1,26 @@
 using System;
-class Q {
-    static void Main() {
+class Q
+{
+    static void Main()
+    {
         int n = Convert.ToInt32(Console.ReadLine()), a = 1, b = 0;
         string q;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
+        {
             q = Console.ReadLine();
             string[] qq = q.Split(' ');
-            if (qq[0] == "*") {
+            if (qq[0] == "*")
+            {
                 a *= Convert.ToInt32(qq[1]);
                 b *= Convert.ToInt32(qq[1]);
             }
-            else if (qq[0] == "+") {
+            else if (qq[0] == "+")
+            {
                 if (qq[1] == "x") a += 1;
                 else b += Convert.ToInt32(qq[1]);
             }
-            else {
+            else
+            {
                 if (qq[1] == "x") a -= 1;
                 else b -= Convert.ToInt32(qq[1]);
             }
