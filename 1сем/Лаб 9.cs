adf306140d3b016839b6e0c0a8c8a0f8ad240ce4
nameofmypony <romanbezverhnij@gmail.com>
2024-12-08 17:22:15 +0600

    Update Лаб 9.cs

--- 1сем/Лаб 9.cs
+++ 1сем/Лаб 9.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Runtime.InteropServices;
 class Q
 {
     static void Main()
@@ -13,11 +12,10 @@ class Q
             maxdlina = 0;
             string l = Console.ReadLine();
             if (l == "") break;
-            char[] letters = l.ToCharArray();
             //определить количество строк, в которых присутствуют сочетания a*b, где * - любой символ
-            for (int i = 0; i < letters.Length - 2; i++)
+            for (int i = 0; i < l.Length - 2; i++)
             {
-                if ((letters[i] == 'a') && (letters[i + 2] == 'b'))
+                if ((l[i] == 'a') && (l[i + 2] == 'b'))
                 {
                     s++;
                     break;
@@ -25,20 +23,20 @@ class Q
             }
             //определить максимальную длину подстроки в каждой строке, состоящей из сочетания abc, при этом необязательно присутствуют все три символа
             //например, подстроки abcabcabc и abca тоже считаются.
-            for (int i = 0; i < letters.Length; i++)
+            for (int i = 0; i < l.Length; i++)
             {
-                for (int j = i; j < letters.Length; j += 3)
+                for (int j = i; j < l.Length; j += 3)
                 {
-                    if (letters[j] == 'a') dlina++;
+                    if (l[j] == 'a') dlina++;
                     else break;
-                    if (j + 1 < letters.Length)
+                    if (j + 1 < l.Length)
                     {
-                        if (letters[j + 1] == 'b') dlina++;
+                        if (l[j + 1] == 'b') dlina++;
                         else break;
                     }
-                    if (j + 2 < letters.Length)
+                    if (j + 2 < l.Length)
                     {
-                        if (letters[j + 2] == 'c') dlina++;
+                        if (l[j + 2] == 'c') dlina++;
                         else break;
                     }
                 }
