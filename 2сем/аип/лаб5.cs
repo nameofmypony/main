2f5f4d8fc9100c3ad69b64e6f073aae79917da3b
nameofmypony <romanbezverhnij@gmail.com>
2025-03-07 12:20:02 +0600

    Create лаб5.cs

---
+++ 2сем/аип/лаб5.cs
@@ -0,0 +1,29 @@
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter numbers separated by spaces");
+        var dictionary = new Dictionary<int, int>();
+        string input = Console.ReadLine();
+        string[] words = input.Split(" ");
+        foreach (string word in words)
+        {
+            if (int.TryParse(word, out int number))
+            {
+                if (dictionary.ContainsKey(number)) dictionary[number]++;
+                else dictionary.Add(number, 1);
+            }
+            else
+            {
+                Console.WriteLine("input error");
+                return;
+            }
+        }
+        Console.WriteLine("number\tcount");
+        foreach (var element in dictionary)
+        {
+            Console.WriteLine(element.Key + "\t" + element.Value);
+        }
+    }
+}
