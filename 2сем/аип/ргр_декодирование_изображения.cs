011de2e61ca47773e89cac82d9e72878cb88955d
nameofmypony <romanbezverhnij@gmail.com>
2025-05-30 19:49:23 +0600

    Update ргр_декодирование_изображения.cs

--- 2сем/аип/ргр_декодирование_изображения.cs
+++ 2сем/аип/ргр_декодирование_изображения.cs
@@ -15,11 +15,9 @@ class Program
         string[] wh = lines[0].Split(" ");
         int w = int.Parse(wh[0]);
         int h = int.Parse(wh[1]);
-
         image = new int[h, w];
         regionsQueue = new Queue<Region>();
         lineIndex = 2;
-
         string secondLine = lines[1].Trim();
         if (secondLine == "Z")
         {
@@ -29,60 +27,46 @@ class Program
         else
         {
             int color = int.Parse(secondLine);
-            for (int i = 0; i < h; i++)
-                for (int j = 0; j < w; j++)
-                    image[i, j] = color;
+            for (int i = 0; i < h; i++) for (int j = 0; j < w; j++) image[i, j] = color;
         }
-
         using StreamWriter sw = new("output.txt");
         for (int i = 0; i < h; i++)
         {
             string line = "";
             for (int j = 0; j < w; j++)
             {
-                if (j > 0)
-                    line += " ";
+                if (j > 0) line += " ";
                 line += image[i, j];
             }
             sw.WriteLine(line);
         }
     }
-
     static void DecodeRegion()
     {
         while (regionsQueue.Count > 0)
         {
             Region current = regionsQueue.Dequeue();
-            
             string[] parts = lines[lineIndex].Split(' ');
             lineIndex++;
-
             int w1 = (current.w + 1) / 2;
             int w2 = current.w / 2;
             int h1 = (current.h + 1) / 2;
             int h2 = current.h / 2;
-
             ProcessPart(parts[0], current.x, current.y, w1, h1);
             ProcessPart(parts[1], current.x + w1, current.y, w2, h1);
             ProcessPart(parts[2], current.x, current.y + h1, w1, h2);
             ProcessPart(parts[3], current.x + w1, current.y + h1, w2, h2);
         }
     }
-
     static void ProcessPart(string part, int x, int y, int w, int h)
     {
         part = part.Trim();
-        
-        if (part == "-" || w == 0 || h == 0)
-            return;
-        else if (part == "Z")
-            regionsQueue.Enqueue(new Region { x = x, y = y, w = w, h = h });
+        if (part == "-" || w == 0 || h == 0) return;
+        else if (part == "Z") regionsQueue.Enqueue(new Region { x = x, y = y, w = w, h = h });
         else
         {
             int color = int.Parse(part);
-            for (int i = y; i < y + h; i++)
-                for (int j = x; j < x + w; j++)
-                    image[i, j] = color;
+            for (int i = y; i < y + h; i++) for (int j = x; j < x + w; j++) image[i, j] = color;
         }
     }
 }
